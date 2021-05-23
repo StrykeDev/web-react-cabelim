22ca38349a71e339deb4a902f01bfe963761d3a4
Barak Attias <attias.barak@live.com>
2021-05-23 15:45:40 +0300

    Added global theme provider

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import "@fontsource/roboto";
 import "./index.css";
 import App from "./component/App";
 
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+import { amber, grey } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: amber,
+    secondary: grey,
+  },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true,
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
